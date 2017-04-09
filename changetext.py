3064f495d5d120be5b7e33a0f68868c642534c43
Jamie <tfung000@citymail.cuny.edu>
2017-04-09 06:43:48 -0400

    added leaderboard

---
+++ changetext.py
@@ -0,0 +1,22 @@
+import random
+
+f = open("test.txt", "w")
+f.write('<table id ="leadertable">')
+f.write('<tr>')
+f.write('<th>Rank</th>')
+f.write('<th>User</th>')
+f.write('<th>Score</th>')
+f.write('</tr>')
+
+for i in range(1,11):
+	s = '<tr id = "rank' + str(i) +'">'
+	f.write(s)
+	r = '<td>' + str(i) + '</td>'
+	f.write(r)
+	q = '<td>' + str(random.random()) + '</td>'
+	f.write(q)
+	score = '<td>' + str(random.randrange(100)) + '</td>'
+	f.write(score)
+	f.write('</tr>')
+
+f.write('</table>')
