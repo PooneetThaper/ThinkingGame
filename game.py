b7966289b2a1afe2e617c776ddb2cd5d291f46d1
Jamie <tfung000@citymail.cuny.edu>
2017-04-09 06:50:02 -0400

    added game.py

---
+++ game.py
@@ -0,0 +1,23 @@
+import random
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import api.py
+
+maxRound = 5
+currentRound = 1
+
+def setUpGame():
+	allPhotos = getAllPaths()
+	randomIndices = []
+	randomPhotos = []
+
+	while(len(randomIndices) < maxRound):
+		index = random.randrange(len(allPhotos))
+		randomPhotos.append(allPhotos[index])
+
+	nextRound(allPhotos)
+
+def nextRound(allPhotos):
+	#send time request to flask server
+	allTags = getAllTags(allPhotos[currentRound])
+	currentRound +=1
